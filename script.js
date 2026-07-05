234edeb4aa15c6ca8b94ca26c5482d3e522f9d43
kowalsking <kovvalsking@gmail.com>
2026-07-05 22:11:09 +0300

    unit 4. ex

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ import * as THREE from 'three'
 const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene()
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
 })
 const mesh = new THREE.Mesh(geometry, material)
+
+mesh.position.set(0.7, -0.6, 1)
+mesh.scale.set(2, 0.25, 0.5)
+
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
 scene.add(mesh)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 const sizes = {
   width: 800,
@@ -17,9 +30,9 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1.3
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
